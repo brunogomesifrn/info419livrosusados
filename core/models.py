f9384b959a3c81196bc739135b88bda92d6152f7
NataliaCrisTM <nataliactertum@gmail.com>
2019-06-24 13:11:51 -0300

    iniciando

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+	
+class Livro(models.Model):
+	nome = models.CharField('Nome', max_length=100)
+	autor = models.CharField('autor', max_length=100)
+	preço = models.DecimalField('preço', max_digits=5, decimal_places=2, null = True)
+	
+		
 
 # Create your models here.
--- core/views.py
+++ core/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Livro
+from .forms import LivroForm
 # Create your views here.
+def index(request):	
+	return render(request, 'index.html')
+
+def cadLiv(request):
+	form = LivroForm(request.POST or None, request.FILES or None)
+	if form.is_valid():
+		form.save()
+		return redirect(Livro)
+	contexto = {
+	'form': form
+	}
+	return render(request, 'cadLiv.html', contexto)
