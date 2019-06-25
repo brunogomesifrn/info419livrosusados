cc5decef19fd27ae67a81f5d640e9734f0995a48
Milly-y <milenna.milly.nunes2716@gmail.com>
2019-06-24 21:17:00 -0300

    adicionei um model chamado imagem

--- core/models.py
+++ core/models.py
@@ -2,8 +2,9 @@ from django.db import models
 	
 class Livro(models.Model):
 	nome = models.CharField('Nome', max_length=100)
-	autor = models.CharField('autor', max_length=100)
-	preço = models.DecimalField('preço', max_digits=5, decimal_places=2, null = True)
+	autor = models.CharField('Autor', max_length=100)
+	preço = models.DecimalField('Preço', max_digits=5, decimal_places=2, null = True)
+	imagem = models.ImageField('Imagem', upload_to='media/', max_length=100)
 	
 		
 
